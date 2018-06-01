3cc8632c466296226d80835bd70d07015c5683a1
syamsir11 <zem.cisco@gmail.com>
2018-06-01 18:50:02 +0800

    Dashboard

---
+++ application/modules/Dashboard/controllers/Dashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+	/**
+	* 
+	*/
+	class Dashboard extends MX_Controller
+	{
+		function __construct()
+		{
+			$this->load->library('Trayek');
+		}
+		public function index()
+		{
+			echo modules::run('Layout/Layout/index',['page'=>'grafik']);
+		}
+	}
+?>
